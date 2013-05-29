41e63d04e36f6b7b9185b8615660ee09df7fe154
Ben Corlett <bencorlett@me.com>
2013-05-29 16:39:33 +1000

    Adding service provider.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

---
+++ src/Cartalyst/Widgets/WidgetsServiceProvider.php
@@ -0,0 +1,55 @@
+<?php namespace Cartalyst\Widgets;
+/**
+ * Part of the Platform application.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Platform
+ * @version    2.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class WidgetsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
+
+		$blade->extend(function($value) use ($blade)
+		{
+			$matcher = $blade->createMatcher('widget');
+
+			return preg_replace($matcher, '<?php echo app(\'widgets\')->make$2; ?>', $value);
+		});
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['widgets'] = $this->app->share(function($app)
+		{
+			return new WidgetResolver($app, $app['extensions']);
+		});
+	}
+
+}
